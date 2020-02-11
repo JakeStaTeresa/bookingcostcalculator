ecbfa6bfe6f1dd0e9f40e3bacd617d38369fe597
Kriztian Jake Sta Teresa <jake@barcodeessentials.com>
2020-02-12 01:15:08 +1100

    Refactored booking cost calculation logic in its own function. Add basic logging.

--- BookingCostCalculator.Application/Queries/GetBookingCostsQuery.cs
+++ BookingCostCalculator.Application/Queries/GetBookingCostsQuery.cs
@@ -7,6 +7,7 @@ using BookingCostCalculator.Application.Calculation;
 using BookingCostCalculator.Application.Validation;
 using BookingCostCalculator.Domain;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace BookingCostCalculator.Application.Queries
 {
@@ -16,43 +17,58 @@ namespace BookingCostCalculator.Application.Queries
         
         public class GetBookingCostsQueryHandler : IRequestHandler<GetBookingCostsQuery, IEnumerable<Booking>>
         {
+            private readonly ILogger<GetBookingCostsQueryHandler> logger;
             private readonly IEnumerable<IValidator> validators;
             private readonly IEnumerable<ICalculator> calculators;
-            public GetBookingCostsQueryHandler(IEnumerable<IValidator> validators, IEnumerable<ICalculator> calculators)
+            public GetBookingCostsQueryHandler(ILogger<GetBookingCostsQueryHandler> logger, IEnumerable<IValidator> validators, IEnumerable<ICalculator> calculators)
             {
+                this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
                 this.validators = validators ?? throw new ArgumentNullException(nameof(validators));
                 this.calculators = calculators ?? throw new ArgumentNullException(nameof(calculators));
+                
+                logger.LogInformation("Starting GetBookingCostsQueryHandler");
+                logger.LogInformation("Validators will be executed in the following order: ");
+                foreach (var validator in validators)
+                {
+                    logger.LogInformation($" * {validator.GetType().Name}");
+                }
+                
+                logger.LogInformation("Calculators will be evaluated in the following order: ");
+                foreach (var calculator in calculators)
+                {
+                    logger.LogInformation($" * {calculator.GetType().Name}");
+                }
             }
             
             public Task<IEnumerable<Booking>> Handle(GetBookingCostsQuery request, CancellationToken cancellationToken)
             {
-                var returnValue = request.Bookings.Select(b =>
-                {
-                    //Booking must past all validators
-                    var isValid = validators
-                        .Select(v => v.Validate(b))
-                        .All(r => r);
-
-                    var cost = 0m;
-                    if (isValid)
-                    {
-                        // Booking will perform calculation using the first applicable calculator.
-                        // Calculators are in dependency registration order 
-                        var calculator = calculators.First(c => c.IsApplicable(b));
-                        cost = calculator.Calculate(b);
-                    }
-
-                    return new Booking
-                    {
-                        Id = b.Id,
-                        From = b.From,
-                        To = b.To,
-                        IsValid = isValid,
-                        Cost = cost
-                    };
-                });
-                
+                var returnValue = request.Bookings.Select(CalculateCost);
                 return Task.FromResult(returnValue);
+            }
+
+            private Booking CalculateCost(Booking booking)
+            {
+                var isValid = validators
+                    .Select(v => v.Validate(booking))
+                    .All(r => r);
+
+                var cost = 0m;
+                if (isValid)
+                {
+                    // Calculator with highest applicable rate is applied. This is dictated
+                    // by dependency registration order
+                    var calculator = calculators.First(c => c.IsApplicable(booking));
+                    cost = calculator.Calculate(booking);
+                }
+
+                return new Booking
+                {
+                    Id = booking.Id,
+                    From = booking.From,
+                    To = booking.To,
+                    IsValid = isValid,
+                    Cost = cost
+                };
             }
         }
     }
